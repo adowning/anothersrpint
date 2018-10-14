8c50ffe63f13d5cd26374f0488bf962996e2554b
adowning <ashdowning@gmail.com>
2018-10-13 23:37:24 -0500

    adsfdsf

--- mobile/serverless/local-debuging/functions/zip/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-zip -rq function.zip functionAsynch.js package.json node_modules
-bx wsk action create zippedFunctionAsynch --kind nodejs:8 function.zip
-#bx wsk action update zippedFunctionAsynch --kind nodejs:8 function.zip
-rm function.zip
